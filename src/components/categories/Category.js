43734fc4533c0c43a057a5af3a3fe4fb117d5544
gugaime <gugaime@gmail.com>
2018-12-05 23:49:53 -0200

    alterando design do post

--- src/components/categories/Category.js
+++ src/components/categories/Category.js
@@ -13,7 +13,7 @@ const Category =  (props) => {
               <img alt="foto" className="photo" src={"/emergency.ico"} width={45} />
             </div>
             <div className="category-header-text left">
-              <h4>liquidez diaria</h4>
+              <h4>com liquidez diaria</h4>
               <p>Ideal para montar um fundo de emergencia</p>
             </div>
           </div>
@@ -28,12 +28,12 @@ const Category =  (props) => {
               <img alt="foto" className="photo" src={"/clock2.png  "} width={45} />
             </div>
             <div className="category-header-text left">
-              <h4>pagamento mensal</h4>
+              <h4>com pagamento mensal</h4>
               <p>Investimentos que pagam juros mensalmente.</p>
             </div>
           </div>
           <div className="category-body">
-            <h4 className="">101% cdi</h4>
+            <h4 className="">100% cdi</h4>
           </div>
         </div>
 
@@ -48,8 +48,48 @@ const Category =  (props) => {
             </div>
           </div>
           <div className="category-body">
-            <h4 className="">12,8%  ----  101% cdi  ----  ipca+5,6%</h4>
+            <table className="category-table">
+              <tr>
+                <td>6 meses</td>
+                <td>103% cdi</td>
+              </tr>
+              <tr>
+                <td>1 ano</td>
+                <td>112% cdi</td>
+              </tr><tr>
+                <td>2 anos</td>
+                <td>115% cdi</td>
+              </tr>
+            </table>
+          </div>          
+        </div>
+
+         <div className="card category hover animated">
+          <div className="category-header">
+            <div className="category-header-image">
+              <img alt="foto" className="photo" src={"/home.png  "} width={45} />
+            </div>
+            <div className="category-header-text left">
+              <h4>médio prazo</h4>
+              <p>De 2 anos a 5 anos de vencimento.</p>
+            </div>
           </div>
+          <div className="category-body">
+            <table className="category-table">
+              <tr>
+                <td>6 meses</td>
+                <td>103% cdi</td>
+              </tr>
+              <tr>
+                <td>1 ano</td>
+                <td>112% cdi</td>
+              </tr><tr>
+                <td>2 anos</td>
+                <td>115% cdi</td>
+              </tr>
+            </table>
+          </div>
+          
         </div>
         
     </div>
