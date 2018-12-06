c143f03b94c513f248d5d097bc33cc30301da6af
gugaime <gugaime@gmail.com>
2018-12-06 00:03:42 -0200

    novo quickviewer

--- src/components/quickviewer/QuickViewer.js
+++ src/components/quickviewer/QuickViewer.js
@@ -6,19 +6,19 @@ const QuickViewer =  (props) => {
     let data = props.data;
     let assets = props.data.assets.map( (a,i) => {
         return (
-            <div key={i} className="recommend" id="recommend3">
-            <h4>{a.tipo}</h4>
-            <h5>{a.texto}</h5>
-        </div>);
+        <tr key={i} >
+            <td>{a.tipo}</td>
+            <td>{a.texto}</td>
+        </tr>);
     });
 
      
    return (
     <div className="quickviwer">
         <h4 className="recommend-title">Melhores <strong>{data.type}</strong> hoje</h4>
-        <div className="list-recommend">
+        <table className="list-recommend">
             {assets}
-        </div>
+        </table>
     </div>
     );
     
